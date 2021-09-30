a2aa81fb8b286529b9e5900618f3c4f0fe01e156
AdelAziza <adelazizauseni@gmail.com>
2021-09-30 12:21:51 -0700

    Done with the Database

---
+++ BusinessQuestions.sql
@@ -0,0 +1,214 @@
+Use Hotel_Project
+
+with hotelsReport as (
+select * from [Report-2018]
+
+UNION
+
+select * from [Report-2019]
+
+UNION
+
+select * from [Report-2020])
+
+select stays_in_week_nights + stays_in_weekend_nights as AddingWeek_Weekend from hotelsReport
+
+
+---Questions the owner of the hotel wants to know
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
+
+---Question 1 :
+
+----Query
+
+
+
+----Answer
+
+
+
+
